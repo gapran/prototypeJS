a6d88f7767c28fe294b3a48065af416f730b60d4
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-06-05 21:33:39 +0200

    Adds image function signatures

--- imports/ui/image/image.js
+++ imports/ui/image/image.js
@@ -3,3 +3,12 @@ import { Template } from "meteor/templating";
 Template.image.helpers({
 
 });
+
+
+function setImage(fileName){}
+function setBox(){}
+function setSize(width,  height){}
+function setHover(text){}
+function setAppear(){}
+function setDisappaer(){}
+function noBorder(){}
