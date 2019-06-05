2cd5fd54abd70409505107970f9d40e59f9d6223
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-06-05 23:14:30 +0200

    Adds click events for 3 badges

--- imports/ui/image/image.js
+++ imports/ui/image/image.js
@@ -1,8 +1,20 @@
-import { Template } from "meteor/templating";
+import {Template} from "meteor/templating";
 
+Template.badge1.events({
+
+    'click #levelImg': function () {
+        console.log("The badge 1 image is clicked.");
+    },
+});
+Template.badge2.events({
+
+    'click #levelImg': function () {
+        console.log("The badge 2 image is clicked.");
+    },
+});
 Template.badge3.events({
 
-    'click #levelImg': function() {
+    'click #levelImg': function () {
         console.log("The badge 3 image is clicked.");
     },
 
