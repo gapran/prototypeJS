cda9d494c7a14f4d6143ffe91d5a997c2460b0d9
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-06-11 23:03:33 +0200

    Retrieves Table Headers from MongoDB

--- imports/ui/table/table.js
+++ imports/ui/table/table.js
@@ -1,10 +1,16 @@
-import { Projects } from "../../api/projects.js";
-import { Template } from "meteor/templating";
+import {Projects} from "../../api/projects.js";
+import {Template} from "meteor/templating";
 
 Template.bodyComponent.helpers({
-  bugsList()
-  {
-      return Projects.find({},{sort: {Progress: 1} });
-  }
+    bugsList() {
+        return Projects.find({name: {$exists: true}}, {sort: {Progress: 1}});
+    }
 });
 
+Template.headerComponent.helpers(
+    {
+        headersList() {
+            return Projects.find({header: {$exists: true}});
+        }
+
+    });
