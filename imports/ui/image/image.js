ed24ba061dac707f4ce3e0bca457bb0048aff7dc
Lisa Nguyen <lisa.nqd@gmail.com>
2019-06-11 15:49:30 +0200

    Fixing Codacy issues

--- imports/ui/image/image.js
+++ imports/ui/image/image.js
@@ -2,7 +2,7 @@ import {Template} from "meteor/templating";
 
 Template.badge.events({
 
-  'click': function () {
+  click() {
       sAlert.info("Image \"" + this.alt + "\" clicked.", {timeout: 2000});
    },
 });
