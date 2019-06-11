1e4023a0ee4160dff05e2225f2f6a5eb668d6969
Lisa Nguyen <lisa.nqd@gmail.com>
2019-06-11 11:21:47 +0200

    Update image.js

--- imports/ui/image/image.js
+++ imports/ui/image/image.js
@@ -1,21 +1,8 @@
 import {Template} from "meteor/templating";
 
-Template.badge1.events({
+Template.badge.events({
 
-    'click #levelImg': function () {
-        console.log("The badge 1 image is clicked.");
+    'click': function () {
+        alert("Click: " + this.alt);
     },
-});
-Template.badge2.events({
-
-    'click #levelImg': function () {
-        console.log("The badge 2 image is clicked.");
-    },
-});
-Template.badge3.events({
-
-    'click #levelImg': function () {
-        console.log("The badge 3 image is clicked.");
-    },
-
 });
