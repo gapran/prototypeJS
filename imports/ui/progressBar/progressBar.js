f64e0644a9262122edaa026ed3c319f39454cfbf
Lisa Nguyen <lisa.nqd@gmail.com>
2019-06-11 11:44:14 +0200

    Progress bar

--- imports/ui/progressBar/progressBar.js
+++ imports/ui/progressBar/progressBar.js
@@ -1,9 +1,12 @@
 import { Template } from "meteor/templating";
 
-
     Template.progressBar.helpers({
         Progress : function()
         {
-            return "70%";
+            return this.val;
+        },
+        Percent : function()
+        {
+            return this.val * 100 / (this.max - this.min);
         },
     });
