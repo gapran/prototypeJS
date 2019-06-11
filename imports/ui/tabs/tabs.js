0e0bb4a4c29c0d07317af8866b2473372c24a0c8
Lisa Nguyen <lisa.nqd@gmail.com>
2019-06-11 18:53:52 +0200

    Fixing Codacy issues

--- imports/ui/tabs/tabs.js
+++ imports/ui/tabs/tabs.js
@@ -2,29 +2,31 @@ import {Template} from "meteor/templating";
 
 Template.tabs.events({
 
-  "click .tabHeader": function(e){
+  "click .tabHeader"(e){
+
       // Mark other tabs as inactive.
       var headers = e.target.parentElement.getElementsByClassName("tabHeader");
-      for (i = 0; i < headers.length; i++) {
-        headers[i].className = headers[i].className.replace(" active", "");
+      for (var i = 0; i < headers.length; i++) {
+        var headerStyle = headers[i].className;
+        headers[i].className = headerStyle.replace(" active", "");
       }
 
       // Mark tab as active.
       e.target.className += " active";
 
       // Make the other contents invisible.
-      for (i = 0; i < this.tabData.length; i++) {
-        var tabElement = document.getElementById(this.tabData[i].id);
+      for (var i = 0; i < this.tabData.length; i++) {
+        var tabElementId = this.tabData[i].id;
+        var tabElement = document.getElementById(tabElementId);
         tabElement.style.display = "none";
       }
-      var tabElement = document.getElementById(e.target.getAttribute("tabId"));
 
       // Place the content in the main container.
+      var mainElement = document.getElementById(e.target.getAttribute("tabId"));
       var tabContainer = e.target.parentElement.parentElement.getElementsByClassName("tabContent")[0];
-      tabContainer.appendChild(tabElement);
+      tabContainer.appendChild(mainElement);
 
       // Make the content visible.
-      tabElement.style.display = "block";
-
+      mainElement.style.display = "block";
   },
 });
