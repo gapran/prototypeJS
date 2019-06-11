f9404b86d3e5f65eff90ca3f05e393705fda1572
Lisa Nguyen <lisa.nqd@gmail.com>
2019-06-11 14:43:55 +0200

    Notifications

--- imports/ui/image/image.js
+++ imports/ui/image/image.js
@@ -3,6 +3,7 @@ import {Template} from "meteor/templating";
 Template.badge.events({
 
     'click': function () {
-        alert("Click: " + this.alt);
-    },
+       // alert("Click: " + this.alt);
+        sAlert.info("Image \"" + this.alt + "\" clicked.", {timeout: 2000});
+     },
 });
