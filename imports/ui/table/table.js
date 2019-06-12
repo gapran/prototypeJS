1e06260a1525721b6897f57171183de996a4dccd
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-06-12 13:53:27 +0200

    Inputs user headers for table
    WIP

--- imports/ui/table/table.js
+++ imports/ui/table/table.js
@@ -1,16 +1,34 @@
 import {Projects} from "../../api/projects.js";
 import {Template} from "meteor/templating";
 
+
+Template.tableHeader.rendered = function () {
+
+    console.log(this.data.heads);
+
+    const headerValues = this.data.heads;
+
+    headerValues.forEach(headerFunction);
+
+};
+
+function headerFunction() {
+}
+
 Template.bodyComponent.helpers({
     bugsList() {
+
         return Projects.find({name: {$exists: true}}, {sort: {Progress: 1}});
     }
 });
 
-Template.headerComponent.helpers(
-    {
-        headersList() {
-            return Projects.find({header: {$exists: true}});
-        }
-
-    });
+// Template.headerComponent.helpers(
+//     {
+//         // headersList() {
+//         //     return Projects.find({header: {$exists: true}});
+//         // }
+//
+//         headersList(){
+//             return headersConst;
+//         }
+//     });
