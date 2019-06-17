1aad30fa98e1d36ae8b3f1da4cd38f3699671cbc
Haseeb Ahmad <haseeb26@mail.uni-paderborn.de>
2019-06-17 23:59:41 +0200

    Updating Jump to another Layout

--- client/main.js
+++ client/main.js
@@ -1,14 +1,3 @@
 import "../imports/ui/prototype1/body.js";
 
 
-
-
-////  Routing  ////
-/*
-Router.route('/', function () {
-    this.render('container', {
-      data: function () { return Items.findOne({_id: this.params._id}); }
-    });
-  });
-*/
-//Router.configure('noRoutesTemplate');
--- imports/ui/image/image.js
+++ imports/ui/image/image.js
@@ -3,12 +3,8 @@ import {Template} from "meteor/templating";
 Template.badge.events({
 
   click() {
-      //sAlert.info("Image \"" + this.alt + "\" clicked.", {timeout: 2000});
+      sAlert.info("Image \"" + this.alt + "\" clicked.", {timeout: 2000});
       //var myWindow = window.open("./imports/ui/imageLayout/imageLayout.html", "_self");
       window.location.replace("/imageLayout.html");
-      //location.href='imports/ui/imageLayout/imageLayout.html'
-      //window.location.href = '/imageLayout.html'
-
-      //FlowRouter.go('/imageLayout');
    },
 });
