6f12819a34e8347a6ff14abeefd1314f4fe29419
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-06-18 12:53:15 +0200

    Retrieves table headers by events

--- imports/ui/table/table.js
+++ imports/ui/table/table.js
@@ -5,6 +5,28 @@ import {Template} from "meteor/templating";
 Template.bodyComponent.helpers({
 
     bugsList() {
-        return Projects.find({name: {$exists: true}}, {sort: {Progress: 1}});
+        return Projects.find({name: {$exists: true}});
     }
+});
+
+Template.tableElement.events({
+
+    'click #nameID': function () {
+
+        var hello = document.getElementById("nameID");
+        console.log(hello.innerText);
+    },
+
+    'click #statusID': function () {
+
+        var hello = document.getElementById("statusID");
+        console.log(hello.innerText);
+    },
+
+    'click #progressID': function () {
+
+        var hello = document.getElementById("progressID");
+        console.log(hello.innerText);
+    }
+
 });
