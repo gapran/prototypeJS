625a72e0a79360f66fa752cd3f54ced36bdb4b11
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-06-18 11:10:32 +0200

    Retrieves row values and Cleans table library

--- imports/ui/table/table.js
+++ imports/ui/table/table.js
@@ -2,35 +2,6 @@ import {Projects} from "../../api/projects.js";
 import {Template} from "meteor/templating";
 
 
-Template.tableHeader.rendered = function () {
-
-    console.log(this.data.headers);
-    console.log(this.data.keyNames);
-    console.log(this.data.keyNames[0]);
-
-    var keyNamesFull;
-    keyNamesFull = "";
-
-    for (let i = 0; i < this.data.keyNames.length; i++) {
-
-        keyNamesFull = keyNamesFull + " <td>{{" + this.data.keyNames[i] + "}}</td> ";
-
-        Session.set("keyNamesHTML", keyNamesFull);
-
-        // return keyNamesFull;
-    }
-};
-
-
-Template.bugsListRow.helpers({
-
-    renderKeysHTML: function () {
-
-        //return Session.get("keyNamesHTML");
-        return "<b>{{name}}</b>";
-    }
-});
-
 Template.bodyComponent.helpers({
 
     bugsList() {
