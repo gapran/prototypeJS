0f3d189fca7be256ae84fb5f7a641aeff0c67c18
Lisa Nguyen <lisa.nqd@gmail.com>
2019-06-18 17:29:49 +0200

    Table + sorting, and moved hardcoded values to js file.

--- imports/ui/prototype1/prototype1.js
+++ imports/ui/prototype1/prototype1.js
@@ -1,5 +1,8 @@
 // Libraries
 
+import {Projects} from "../../api/projects.js";
+import {Template} from "meteor/templating";
+
 import "../progressBar/progressBar.html";
 import "../progressBar/progressBar.js";
 
@@ -19,3 +22,25 @@ import "../image/image.js";
 // Prototype 1
 
 import "./prototype1.html";
+
+
+Template.prototype1.helpers({
+
+  // Bar chart data
+  proficiencyBarChartLabels: ["Bronze", "Silver", "Gold", "Master", "Profesional"],
+  proficiencyBarChartValues: [20, 40, 50, 80, 95],
+
+  // Tab data
+  bottomTabData: [
+    {label:"Analyze", id:"tabAnalyze"},
+    {label:"Categorize", id:"tabCategorize"},
+    {label:"Highlights", id:"tabHighlights"},
+    {label:"Monitor & Visualize", id:"tabMonitorVisualize"}
+  ],
+
+  // Table data
+  resultsTableLabels: ["Name", "Status", "Progress"],
+  resultsTableData: Projects.find({}),
+
+});
+
