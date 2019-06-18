98293c42ed7cfd3ab14e88aabeb58088989dd6f0
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-06-18 14:03:09 +0200

    Sorts progress column

--- imports/ui/table/table.js
+++ imports/ui/table/table.js
@@ -5,9 +5,21 @@ import {Template} from "meteor/templating";
 Template.bodyComponent.helpers({
 
     bugsList() {
-        return Projects.find({name: {$exists: true}});
+        alert(Session.get("sort_query"));
+        return Projects.find({});
     }
 });
+
+Template.tableElement.rendered = function () {
+
+    var sortIt = {$regex: Session.get("sort_query")};
+    alert(sortIt);
+    if (sortIt === "Progress") {
+        return Projects.find({}, {sort: {sortIt: 1}});
+    } else {
+        return Projects.find({});
+    }
+};
 
 Template.tableElement.events({
 
@@ -15,18 +27,78 @@ Template.tableElement.events({
 
         var hello = document.getElementById("nameID");
         console.log(hello.innerText);
+        Session.set("sort_query", hello.innerText);
+
     },
 
     'click #statusID': function () {
 
         var hello = document.getElementById("statusID");
         console.log(hello.innerText);
+        Session.set("sort_query", hello.innerText);
+
     },
 
     'click #progressID': function () {
 
         var hello = document.getElementById("progressID");
         console.log(hello.innerText);
+        Session.set("sort_query", hello.innerText);
+        console.log("selected sort name is " + Session.get("sort_query"));
+
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.getElementById("bugsTable");
+        switching = true;
+        //Set the sorting direction to ascending:
+        dir = "asc";
+        /*Make a loop that will continue until
+        no switching has been done:*/
+        while (switching) {
+            //start by saying: no switching is done:
+            switching = false;
+            rows = table.rows;
+            /*Loop through all table rows (except the
+            first, which contains table headers):*/
+            for (i = 1; i < (rows.length - 1); i++) {
+                //start by saying there should be no switching:
+                shouldSwitch = false;
+                /*Get the two elements you want to compare,
+                one from current row and one from the next:*/
+                x = rows[i].getElementsByTagName("TD")[2];
+                y = rows[i + 1].getElementsByTagName("TD")[2];
+                /*check if the two rows should switch place,
+                based on the direction, asc or desc:*/
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                /*If a switch has been marked, make the switch
+                and mark that a switch has been done:*/
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                //Each time a switch is done, increase this count by 1:
+                switchcount++;
+            } else {
+                /*If no switching has been done AND the direction is "asc",
+                set the direction to "desc" and run the while loop again.*/
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
     }
+
 
 });
