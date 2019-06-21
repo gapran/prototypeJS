ea3b1b32c7c73749f94aa30cdde7d73352111b51
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-06-21 09:29:06 +0200

    Displays matched Table Rows
    + matching: checked items

--- imports/ui/filter/filter.js
+++ imports/ui/filter/filter.js
@@ -4,7 +4,6 @@ Template.filter.events({
     "click .filterValue"(e) {
 
         const filtersNumber = document.getElementsByClassName("filterValue").length;
-        console.log(filtersNumber);
 
         //var filterList = document.getElementsByClassName("filterValue");
         // for(var i = 0; i < filtersNumber; i++) {
@@ -21,7 +20,6 @@ Template.filter.events({
                 selectedList.push(filterList[p].value);
             }
         }
-        console.log(filterList);
         console.log(selectedList);
 
         // If all filters are selected - default
@@ -50,29 +48,37 @@ Template.filter.events({
             }
         }
 
+        // Retrieve the table to be filtered
+        const table = document.getElementById(this.tableID);
+        const rows = table.rows;
 
-        // Compare each value in selectedList ( list of checked items ) with the values in table
-        for (let i = 0; i < selectedList.length; i++) {
+        // Loop through all rows in a table (except the first one, which contains the table headers)
+        for (let j = 1; j < rows.length; j++) {
 
-            const filterValue = selectedList[i];
+            // Boolean to whether show the current row or not
+            let showRow = false;
+            // Get the row elements you want to compare with current filterValue
+            const columns = table.rows[j].cells.length; // Number of td ( column ) values in a row
+            const rowSet = new Set();
+            for (let k = 0; k < columns; k++) {
+                const tableString = rows[j].getElementsByTagName("TD")[k].innerHTML.toString();
+                rowSet.add(tableString);
+            }
 
-            const table = document.getElementById(this.tableID);
-            const rows = table.rows;
+            // Compare each value in selectedList ( list of checked items ) with the values in current table row
+            for (let i = 0; i < selectedList.length; i++) {
 
-            // Loop through all rows in a table (except the first one, which contains the table headers)
-            for (let j = 1; j < rows.length; j++) {
-                // Get the row elements you want to compare with current filterValue
-                const columns = table.rows[j].cells.length; // Number of td ( column ) values in a row
-                const rowSet = new Set();
-                for (let k = 0; k < columns; k++) {
-                    const tableString = rows[j].getElementsByTagName("TD")[k].innerHTML.toString();
-                    rowSet.add(tableString);
+                const filterValue = selectedList[i];
+                if (rowSet.has(filterValue)) {
+                    // rows[j].style.display = "none";
+                    showRow = true;
                 }
-                if (!rowSet.has(filterValue)) {
-                    rows[j].style.display = "none";
-                } else {
-                    rows[j].style.display = "table-row";
-                }
+            }
+            // Display the table row if it has one of the selected ( checked ) item
+            if (showRow === true) {
+                rows[j].style.display = "table-row";
+            } else {
+                rows[j].style.display = "none";
             }
         }
 
