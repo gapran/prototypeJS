2a7bc29e48a0ea3f80894299595a33292f1444a8
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-06-21 15:59:57 +0200

    Fixes Cyclomatic complexity

--- imports/ui/filter/filter.js
+++ imports/ui/filter/filter.js
@@ -12,67 +12,64 @@ Template.filter.events({
 
         // Retrieve the selected item
         const selectedItem = event.target.getAttribute("value");
+        // List of items in a filter
+        const filterList = document.getElementsByTagName("input");
 
         if (selectedItem === "Select all") {
 
-            const checkList = document.getElementsByTagName("input");
-            for (let element of checkList) {
+            for (let element of filterList) {
                 // All filter items are checked
                 element.checked = true;
             }
-        }
+        } else if (selectedItem === "Deselect all") {
 
-        if (selectedItem === "Deselect all") {
-
-            const checkList = document.getElementsByTagName("input");
-            for (let element of checkList) {
+            for (let element of filterList) {
                 // All filter items are unchecked
                 element.checked = false;
             }
-        }
+        } else {
 
-        // Get selected ( checked ) filter names
-        const selectedList = [];
-        const filterList = document.getElementsByTagName("input");
-        for (let element of filterList) {
-            if (element.checked) {
-                selectedList.push(element.value);
-            }
-        }
-
-
-        // Loop through all rows in a table
-        for (let row of rows) {
-
-            if (row === table.rows[0]) { // Skips the first one, which contains the table headers
-                continue;
-            }
-
-            // Boolean to whether show the current row or not
-            let showRow = false;
-            // Get the row elements you want to compare with current filterValue
-            // const columns = table.row.cells.length; // Number of td ( column ) values in a row
-            const rowSet = new Set();
-            for (let cell of row.cells) {
-                const tableString = cell.innerText;
-                rowSet.add(tableString);
-            }
-
-            // Compare each value in selectedList ( list of checked items ) with the values in current table row
-            for (let element of selectedList) {
-
-                if (rowSet.has(element)) {
-                    showRow = true;
+            // Get selected ( checked ) filter names
+            const selectedList = [];
+            for (let element of filterList) {
+                if (element.checked) {
+                    selectedList.push(element.value);
                 }
             }
 
-            // Display the table row if it has one of the selected ( checked ) item
-            if (showRow === true) {
-                row.style.display = "table-row";
-            } else {
-                row.style.display = "none";
+
+            // Loop through all rows in a table
+            for (let row of rows) {
+
+                if (row === headerRow) { // Skips the first one, which contains the table headers
+                    continue;
+                }
+
+                // Boolean to whether show the current row or not
+                let showRow = false;
+                // Get the row elements you want to compare with current filterValue
+                // const columns = table.row.cells.length; // Number of td ( column ) values in a row
+                const rowSet = new Set();
+                for (let cell of row.cells) {
+                    const tableString = cell.innerText;
+                    rowSet.add(tableString);
+                }
+
+                // Compare each value in selectedList ( list of checked items ) with the values in current table row
+                for (let element of selectedList) {
+
+                    if (rowSet.has(element)) {
+                        showRow = true;
+                    }
+                }
+
+                // Display the table row if it has one of the selected ( checked ) item
+                if (showRow === true) {
+                    row.style.display = "table-row";
+                } else {
+                    row.style.display = "none";
+                }
             }
         }
-
     }
 });
