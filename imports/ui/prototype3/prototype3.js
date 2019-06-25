5046a42ca2be3269d5cb877e3d9e37ffd9ba5c4a
Lisa Nguyen <lisa.nqd@gmail.com>
2019-06-25 14:03:15 +0200

    Minor cleanup

--- imports/ui/prototype3/prototype3.js
+++ imports/ui/prototype3/prototype3.js
@@ -3,24 +3,8 @@
 import {Projects} from "../../api/projects.js";
 import {Template} from "meteor/templating";
 
-import "../progressBar/progressBar.html";
-import "../progressBar/progressBar.js";
-
-import "../table/table.html";
-import "../table/table.js";
-
-import "../charts/Chart.js";
-import "../charts/barChart.html";
-import "../charts/barChart.js";
-
-import "../tabs/tabs.html";
-import "../tabs/tabs.js";
-
 import "../image/image.html";
 import "../image/image.js";
-
-import "../filter/filter.html";
-import "../filter/filter.js";
 
 // Prototype 3
 import "./prototype3.html";
