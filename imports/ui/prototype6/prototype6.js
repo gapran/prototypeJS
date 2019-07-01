10355a334b74b1f0e3945b5b8e42dceca445bcbb
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-07-01 21:44:25 +0200

    Cleans prototype homepage

--- imports/ui/prototype6/prototype6.js
+++ imports/ui/prototype6/prototype6.js
@@ -4,24 +4,7 @@ import {Template} from "meteor/templating";
 // Prototype 6
 import "./prototype6.html";
 
+Template.prototype6.created = function () {
 
-Template.prototype6.events({
-
-    "click .btn-right-group": function (event) {
-
-        // Prevent default browser form submit
-        event.preventDefault();
-
-        // Get value from element
-        const target = event.target;
-        const text = target.innerText;
-        console.log(text);
-
-        if (text === "User Profile") {
-
-            // console.log("hello");
-            // window.location.href = '/profile';
-        }
-    }
-
-});
+    sAlert.info("Welcome to 'Gamifying Program Analysis' Prototype Builder!", {position: 'bottom', timeout: 3000});
+};
