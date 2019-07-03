82d0208f9f1058336aeecdbc4ba29b9bad216cae
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-07-03 19:24:31 +0200

    Adds actions to chat

--- imports/ui/chat/chat.js
+++ imports/ui/chat/chat.js
@@ -1,1 +1,13 @@
-import { Template } from "meteor/templating";
+import {Template} from "meteor/templating";
+
+Template.chat.events({
+
+    "click .open-button": function () {
+        document.getElementById("chatForm").style.display = "block";
+    },
+
+    "click .btn.cancel": function () {
+        document.getElementById("chatForm").style.display = "none";
+    }
+
+});
