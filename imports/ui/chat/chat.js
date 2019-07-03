bfd7dbecd07ce151ab0aea575b5fcf1e878009ea
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-07-03 21:52:36 +0200

    Adapts ES6 Function notation

--- imports/ui/chat/chat.js
+++ imports/ui/chat/chat.js
@@ -2,11 +2,11 @@ import {Template} from "meteor/templating";
 
 Template.chat.events({
 
-    "click .open-button": function () {
+    "click .open-button"() {
         document.getElementById("chatForm").style.display = "block";
     },
 
-    "click .btn.cancel": function () {
+    "click .btn.cancel"() {
         document.getElementById("chatForm").style.display = "none";
     }
 
