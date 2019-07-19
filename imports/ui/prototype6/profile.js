be38eae4d01cd387990411d8af516254815948ce
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-07-19 15:33:12 +0200

    Adds badge icons
    Credits: Laura Reen
    
    Licence -
    Creative Commons (Attribution-Noncommercial 3.0 Unported)

--- imports/ui/prototype6/profile.js
+++ imports/ui/prototype6/profile.js
@@ -4,7 +4,13 @@
 // File
 
 import "./profile.html";
+import {Template} from "meteor/templating";
 
 Template.prototype6_profile.helpers({
 
 });
+
+Template.prototype6_profile.created = function () {
+
+    sAlert.info("Badge Icons - Credits: Laura Reen, Licence - Creative Commons (Attribution-Noncommercial 3.0 Unported) ", {position: 'bottom', timeout: 3000});
+};
---
+++ public/badges/README.txt
@@ -0,0 +1,4 @@
+Please, copy this code on your website to accredit the author:
+<a href="https://www.iconfinder.com/laurareen">Designed by  Laura Reen</a>
+
+License - Creative Commons (Attribution-Noncommercial 3.0 Unported) 
