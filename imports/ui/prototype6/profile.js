ab497971f3fb83ea37ac7f7612ca983a8b6f0586
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-07-23 00:22:41 +0200

    Adds progress bar
    + percentage of assigned bugs fixed

--- imports/ui/prototype6/profile.js
+++ imports/ui/prototype6/profile.js
@@ -6,6 +6,9 @@
 import "./profile.html";
 import {Template} from "meteor/templating";
 
+import "../progressBar/progressBar.html";
+import "../progressBar/progressBar.js";
+
 Template.prototype6_profile.helpers({
 
 });
@@ -14,3 +17,11 @@ Template.prototype6_profile.created = function () {
 
     sAlert.info("Badge Icons - Credits: Laura Reen, Licence - Creative Commons (Attribution-Noncommercial 3.0 Unported) ", {position: 'bottom', timeout: 3000});
 };
+
+Template.prototype6_profile.events({
+
+    'click .progressArea' () {
+        const value = document.getElementById('progressBar').innerText;
+        sAlert.info("You have fixed "+value+" percent of assigned bugs. Good Job!", { effect: 'stackslide', position: 'bottom-left',timeout: 3000});
+}
+});
