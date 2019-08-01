d9d9ac7b2c3a28ce840cb9f54ce2b6fde16be27a
osamic <osamaalic@gmail.com>
2019-08-01 12:36:35 +0200

    only mobile part , remved the alerts of badegs

---
+++ imports/ui/prototype7/leader.js
@@ -0,0 +1,20 @@
+// Libraries
+// ...
+
+// File
+
+import "./leader.html";
+
+Template.prototype7_leader.helpers({
+
+// Bar chart data
+leaderBarChartData: [
+    // Label for the bar label, and a numeric value
+    {label: "Mike", value: 30},
+    {label: "John", value: 60},
+    {label: "Max", value: 90},
+    {label: "osama", value: 80},
+    {label: "Elina", value: 35}
+]
+
+});
---
+++ imports/ui/prototype7/prototype7.js
@@ -0,0 +1,12 @@
+// Libraries
+
+import {Template} from "meteor/templating";
+// Prototype 7
+import "./prototype7.html";
+import {Projects} from "../../api/projects";
+
+import "../table/table.html";
+import "../table/table.js";
+
+Template.prototype7.created = function () {
+};
