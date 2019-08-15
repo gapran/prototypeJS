95a353fca37e0be5de17e957c1d4574ade295fdb
Haseeb Ahmad <haseeb26@mail.uni-paderborn.de>
2019-08-15 03:10:42 +0200

    Removing Prototype 5

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -15,26 +15,14 @@ import "./prototype2/prototype2.js";
 // Pages of prototype 3
 import "./prototype3/prototype3.js";
 
-// Pages of prototype 5
-import "./prototype5/prototype5.js";
-import "./prototype5/about.js";
-import "./prototype5/login.js";
-
 // Pages of prototype 6
+import "./prototype6/login.js";
 import "./prototype6/prototype6.js";
 import "./prototype6/editor.js";
 import "./prototype6/categorize.js";
 import "./prototype6/highlights.js";
 import "./prototype6/visualize.js";
 
-// Pages of prototype 7
-import "./prototype7/prototype7.js";
-import "./prototype7/profile.js";
-import "./prototype7/activity.js";
-import "./prototype7/community.js";
-import "./prototype7/project.js";
-import "./prototype7/settings.js";
-import "./prototype7/analysis.js";
 
 // Helper for navigation
 Template.body.helpers({
@@ -74,17 +62,6 @@ Template.body.events({
     Session.set("templateName", "prototype3");
   },
 
-  // Navigation rules for prototype 5
-  "click .link_prototype5"() {
-    Session.set("templateName", "prototype5");
-  },
-  "click .link_prototype5_about"() {
-    Session.set("templateName", "prototype5_about");
-  },
-  // "click .loginbutton"() {
-  //   Session.set("templateName", "prototype5");
-  // },
-
   // Navigation rules for prototype 6
   "click .link_prototype6"() {
     Session.set("templateName", "loginPage");
@@ -111,26 +88,4 @@ Template.body.events({
     Session.set("templateName", "editor");
   },
 
-  // Navigation rules for prototype 7
-  "click .link_prototype7"() {
-    Session.set("templateName", "prototype7");
-  },
-  "click .link_prototype7_project"() {
-    Session.set("templateName", "prototype7_project");
-  },
-  "click .link_prototype7_analysis"() {
-    Session.set("templateName", "prototype7_analysis");
-  },
-  "click .link_prototype7_settings"() {
-    Session.set("templateName", "prototype7_settings");
-  },
-  "click .link_prototype7_profile"() {
-    Session.set("templateName", "prototype7_profile");
-  },
-  "click .link_prototype7_activity"() {
-    Session.set("templateName", "prototype7_activity");
-  },
-  "click .link_prototype7_community"() {
-    Session.set("templateName", "prototype7_community");
-  }
 });
--- imports/ui/prototype5/login.js
+++
@@ -1,7 +0,0 @@
-// Libraries
-
-import {Projects} from "../../api/projects.js";
-import {Template} from "meteor/templating";
-
-
-import "./login.html";
