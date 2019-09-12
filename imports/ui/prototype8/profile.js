b4e10b025c1008e220de4fdf671f886bafcebd13
osamic <osamaalic@gmail.com>
2019-09-12 10:02:44 +0200

    add 8

---
+++ imports/ui/prototype8/profile.js
@@ -0,0 +1,31 @@
+// Libraries
+// ...
+
+// File
+
+import "./profile.html";
+import {Template} from "meteor/templating";
+
+import "../progressBar/progressBar.html";
+import "../progressBar/progressBar.js";
+import { Session } from "meteor/session";
+// import "./activity.html";
+// import "./activity.js";
+Template.prototype8_profile.helpers({
+    points(){
+        return Session.get("fix");
+    }
+});
+
+Template.prototype8_profile.created = function () {
+
+    // sAlert.info("Badge Icons - Credits: Laura Reen, Licence - Creative Commons (Attribution-Noncommercial 3.0 Unported) ", {position: 'bottom', timeout: 3000});
+};
+
+Template.prototype8_profile.events({
+
+    "click .progressArea" () {
+        const value = document.getElementById("progressBar").innerText;
+        // sAlert.info("You have fixed "+value+" percent of assigned bugs. Good Job!", { effect: "stackslide", position: "bottom-left",timeout: 3000});
+}
+});
---
+++ imports/ui/prototype8/prototype8.js
@@ -0,0 +1,12 @@
+// Libraries
+
+import {Template} from "meteor/templating";
+// Prototype 8
+import "./prototype8.html";
+import {Projects} from "../../api/projects";
+
+import "../table/table.html";
+import "../table/table.js";
+
+Template.prototype8.created = function () {
+};
